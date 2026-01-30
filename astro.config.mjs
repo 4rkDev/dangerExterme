30c9d99a892df81bdd6f12fa958acba3cb77324d
Jonay Marrero <administracion@nakytec.com>
2026-01-30 16:15:43 +0000

    chore: Install project dependencies and create package.json.

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://dangerextreme.es',
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 1.0,
+      lastmod: new Date(),
+    }),
+  ],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  compressHTML: true,
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: 'esbuild',
+    },
+  },
+});
